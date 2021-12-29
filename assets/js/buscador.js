5dedc5283ca26d7e85076f7b0d62d5daa63055a3
JCervantesB <imjcervantes@gmail.com>
2021-12-29 12:21:06 -0700

    Fix redireccion en el buscador

--- assets/js/buscador.js
+++ assets/js/buscador.js
@@ -10,7 +10,13 @@ function buscarPorFecha(){
     const fechaInput = document.querySelector('#fecha');
     fechaInput.addEventListener('input', function(e) {
         const fechaSeleccionada = e.target.value;
+        
+        if(!fechaSeleccionada) {
+            window.location = `admin`;
+        } else {
 
-        window.location = `admin?fecha=${fechaSeleccionada}`;
+            window.location = `admin?fecha=${fechaSeleccionada}`;
+        }
+
     });
 }
